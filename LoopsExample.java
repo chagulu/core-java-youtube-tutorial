4fd12fac831069ac670835608eb255e619792df0
Basanta Swain <basantswainn@gmail.com>
2025-06-29 20:14:26 +0530

    part 4

---
+++ LoopsExample.java
@@ -0,0 +1,31 @@
+public class LoopsExample {
+
+    public static void main (String[] args){
+
+        for (int i = 5; i <=10; i++){
+            System.out.println("Fro loop : " + i);
+        }
+    
+
+    int xyz =10;
+    while(xyz <=5){
+        System.out.println("while loop : " + xyz);
+        xyz ++;
+    }
+
+
+
+
+    int abc =10;
+
+    do {
+        System.out.println("do while loop : " + abc);
+        abc ++;
+    }while (abc <=5 );
+
+    
+    
+}
+
+
+}
